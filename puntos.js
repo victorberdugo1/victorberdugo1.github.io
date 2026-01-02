70f54f1338393cba125a8c4d183defab21c6a29a
Victor Berdugo <victorberdugo1@gmail.com>
2026-01-02 20:45:00 +0100

    thera

--- puntos.js
+++ puntos.js
@@ -163,9 +163,10 @@ var puntos = [
   { id: 147, lat: 20.366667, lng: -89.766667, nombre: "Uxmal", color: "#C9A66B", imagen: "img/uxmal.jpg", video: "https://ghostsinneon.shortpage.live/watch/-A7N673vF40" },
   { id: 148, lat: 21.166667, lng: -89.633333, nombre: "Dzibilchaltun", color: "#D8D2C3", imagen: "img/dzibilchaltun.jpg", video: "https://ghostsinneon.shortpage.live/watch/fgrdvSqevMg" },
   { id: 149, lat: 39.546389, lng: 20.787778, nombre: "Dodona", color: "#7A7F6A", imagen: "img/dodona.jpg", video: "https://ghostsinneon.shortpage.live/watch/z8aWNPoDe-Y" },
-{ id: 150, lat: 38.483333, lng: 22.500000, nombre: "Delphi", color: "#FF0000" },
-{ id: 151, lat: 37.950000, lng: 23.700000, nombre: "The Acropolis", color: "#FF0000" },
-{ id: 152, lat: 36.366667, lng: 25.366667, nombre: "Thera", color: "#FF0000" },
+  { id: 150, lat: 38.483333, lng: 22.500000, nombre: "Delphi", color: "#8B8174", imagen: "img/delphi.jpg", video: "https://ghostsinneon.shortpage.live/watch/0861iZQgqWw" },
+  { id: 151, lat: 37.950000, lng: 23.700000, nombre: "The Acropolis", color: "#E6D8B5", imagen: "img/acropolis.jpg", video: "https://ghostsinneon.shortpage.live/watch/PQM4tfUdu10" },
+  { id: 152, lat: 36.366667, lng: 25.366667, nombre: "Thera", color: "#5E5A57", imagen: "img/thera.jpg", video: "https://ghostsinneon.shortpage.live/watch/7EHbY0N4U4k" },
+
 { id: 153, lat: 35.233333, lng: 26.266667, nombre: "Palaikastro", color: "#FF0000" },
 { id: 154, lat: 37.223194, lng: 38.922422, nombre: "Göbekli Tepe", color: "#FF0000" },
 { id: 155, lat: 29.534797, lng: 0.183495, nombre: "Ksar Draa", color: "#FF0000" },
